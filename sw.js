ed94c13ecb47318f969998120373e01160349f5f
gamzain2-arch <gamzain2@gmail.com>
2026-09-04 15:47:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='auto-oil-pro-v1';
+const const CACHE='auto-oil-pro-v2';
 const ASSETS=['./','./index.html','./manifest.json','./inventory.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
